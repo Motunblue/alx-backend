b33900f62b3df72b9881ee4400ac86c2ef836e64
Motunrayo <ajirotutu4real@gmail.com>
2024-03-31 20:14:13 +0100

    Added task 3

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+from itertools import islice
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Get Hyper Index method
+        """
+        data = self.indexed_dataset()
+        assert index is not None and 0 <= index <= max(data.keys())
+        page_data = list(islice(data.items(), index, index + page_size))
+        return {
+            "index": index,
+            "next_index": max([v[0] for v in page_data]) + 1,
+            "page_size": page_size,
+            "data": [v[1] for v in page_data]
+        }
