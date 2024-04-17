f7049c0129cce5ac44b951b465afcd846b59d476
Motunrayo <ajirotutu4real@gmail.com>
2024-04-17 18:56:48 +0100

    Added i18n

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Flask App"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get Locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """Home route"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
