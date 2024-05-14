ca13088341e890ef8e9d2d242e473e692d3d0c2f
Motunrayo <ajirotutu4real@gmail.com>
2024-05-14 13:02:46 +0100

    Added task 4, 5 & 6'

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Flask App"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from flask_babel import _
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Get Locale"""
+    locale = request.args.get('locale', None)
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user(id):
+    """Retrun the user from users dict"""
+    users = {
+        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+    }
+    return users.get(id, None)
+
+
+@app.before_request
+def before_request():
+    """Before request"""
+    id = request.args.get('login_as', None)
+    if id:
+        g.user = get_user(int(id))
+
+
+@app.route('/')
+def home():
+    """Home route"""
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
